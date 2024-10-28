539e591420853fcb067f99daeb4abcf2aa7af3f6
Potuz <potuz@prysmaticlabs.com>
2024-10-28 09:49:58 -0300

    Helper for Payload Attestation Signing (#13901)

--- beacon-chain/rpc/eth/config/handlers_test.go
+++ beacon-chain/rpc/eth/config/handlers_test.go
@@ -20,6 +20,10 @@ import (
 	"github.com/prysmaticlabs/prysm/v5/testing/require"
 )
 
+// Variables defined in the placeholderFields will not be tested in `TestGetSpec`.
+// These are variables that we don't use in Prysm. (i.e. future hardfork, light client... etc)
+var placeholderFields = []string{"DOMAIN_BEACON_BUILDER", "DOMAIN_PTC_ATTESTER"}
+
 func TestGetDepositContract(t *testing.T) {
 	params.SetupTestConfigCleanup(t)
 	config := params.BeaconConfig().Copy()
@@ -564,6 +568,12 @@ func TestGetSpec(t *testing.T) {
 			case "BLOB_SIDECAR_SUBNET_COUNT_ELECTRA":
 				assert.Equal(t, "103", v)
 			default:
+				for _, pf := range placeholderFields {
+					if k == pf {
+						t.Logf("Skipping placeholder field: %s", k)
+						return
+					}
+				}
 				t.Errorf("Incorrect key: %s", k)
 			}
 		})
